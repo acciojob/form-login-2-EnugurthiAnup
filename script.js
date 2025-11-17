8d2636d02aa7ade607cad997ed56829b97dcfc14
EnugurthiAnup <eanup.736@gmail.com>
2025-11-17 22:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let phone = document.querySelector('#num');
 let mail = document.querySelector('#mail');
 let sub = document.querySelector('#submit');
 
-document.querySelector("form").addEventListener("submit", function(){
+sub.addEventListener("click", function(){
 
     alert(`First Name: ${first.value} Last Name: ${last.value} Phone Number: ${phone.value} Email ID: ${mail.value}`);
 });
