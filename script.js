c2c7cf3cb192e0584f3d321a9e7273a7590fd91d
EnugurthiAnup <eanup.736@gmail.com>
2025-11-17 22:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let first= document.querySelector('#firstname ' )
+let last= document.querySelector('#lastname ' )
+let phone= document.querySelector('#num ' )
+let mail= document.querySelector('#mail')
+let sub= document.querySelector('button' )
+
+sub.addEventListener('click' , ()=>{
+	alert(`First Name: ${first.value} Last Name: ${last.value} Phone Number: ${phone.value} Email ID: ${mail.value}`)
+})
+					
